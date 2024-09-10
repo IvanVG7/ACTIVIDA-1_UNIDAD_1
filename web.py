8b77e89e3bcc49a9f2091ae79e46b10280be6adf
Ivan Jair Vizcarra Gomez <158106662+IvanVG7@users.noreply.github.com>
2024-09-10 20:09:21 +0000

    HTML dinamico

--- web.py
+++ web.py
@@ -24,6 +24,35 @@ class WebRequestHandler(BaseHTTPRequestHandler):
     <p> Query: {self.query_data()}   </p>
 """
 
+def do_GET(self):
+
+    host = self.headers.get('Host')
+    user_agent = self.headers.get('User-Agent')
+
+
+    self.send_response(200)
+
+
+    self.send_header("Content-Type", "text/html")
+    self.send_header("Server", "CustomPythonServer")
+    self.send_header("Date", self.date_time_string())
+    self.end_headers()
+
+
+    self.wfile.write(self.get_response(host, user_agent).encode("utf-8"))
+
+def get_response(self, host, user_agent):
+
+    query_data = self.query_data()
+    autor = query_data.get('autor', 'desconocido')
+
+
+    return f"""
+    <h1>Proyecto: {self.url().path} Autor: {autor}</h1>
+    <p> Host: {host} </p>
+    <p> User-Agent: {user_agent} </p>
+    """
+
 
 if __name__ == "__main__":
     print("Starting server on port 8000")
